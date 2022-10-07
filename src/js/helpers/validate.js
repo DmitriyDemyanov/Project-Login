ba79377fb4b5bddd264ac5d63b1708f8482cc4ee
Dmitriy Demyanov <dimaprostokosmos@gmail.com>
2022-10-07 15:51:37 +0300

    added input error

---
+++ src/js/helpers/validate.js
@@ -0,0 +1,20 @@
+const regExDic = {
+  email: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/,
+  password: /^[0-9a-zA-Z]{4,}$/,
+};
+
+
+/*
+* function validate. Check Input on RegExp provided in regExpDic by input data-required type
+* @ param{HTMLInputElement}  el
+* @returns {Boolean}  - Return true if input valid or not data-reqired attr.
+*/
+export function validate(el) {
+  const regExpName = el.dataset.required;
+  
+  if (!regExDic[regExpName]) return true;
+
+  return regExDic[regExpName].test(el.value);
+};
+
+
